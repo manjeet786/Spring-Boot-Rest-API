645f9c15d639adf2cb8ccd2511a5fd5b09fafe66
Manjeet Choudhary <manjeet786@users.noreply.github.com>
2023-03-03 12:09:11 +0530

    Add files via upload

---
+++ ClientLoginController.java
@@ -0,0 +1,36 @@
+package com.supplierfinance.controller;
+
+import com.supplierfinance.service.ClientLoginService;
+import com.supplierfinance.vo.ClientLoginVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/login")
+public class ClientLoginController {
+
+    private ClientLoginService clientLoginService;
+
+    ClientLoginController (ClientLoginService clientLoginService) {
+        this.clientLoginService = clientLoginService;
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(ClientLoginController.class);
+
+    @PostMapping("/clientInfo")
+    Boolean loginClient(@RequestBody ClientLoginVO clientLoginVO) {
+        logger.info("Line 36 executed"+clientLoginVO);
+        String userName = clientLoginVO.getUserName();
+        String password = clientLoginVO.getPassword();
+        try {
+            return  this.clientLoginService.validateLogin(clientLoginVO);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+}
